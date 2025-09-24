8e0e13bcf36d395311353abfe315a3ee42787d82
gwilliamboyd <gwillboyd@gmail.com>
2025-09-23 23:06:18 -0500

    Changes test h font and cleans up files

--- components/testimonials/Entry.jsx
+++ components/testimonials/Entry.jsx
@@ -1,17 +1,11 @@
 import styles from '@/styles/testimonials.module.css'
-import entries from './entries/entries.json'
-import { Open_Sans } from 'next/font/google'
-
-const open_sans = Open_Sans({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-})
+// import entries from './entries/entries.json'
 
 const Entry = ({ index, review, name }) => {
   return (
     <div
       key={index}
-      className={`${open_sans.className} ${styles.entryWrapper}`}>
+      className={styles.entryWrapper}>
       <p className={styles.entryReview}>{review}</p>
       <h4 className={styles.entryName}>{name}</h4>
     </div>
